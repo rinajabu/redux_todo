34f19ed3685fe9d3d946723f885a1d7df8b53d91
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-17 15:01:37 -0400

    redux set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+// npx create-react-app . --template redux
 import React from 'react';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
-      REDUX-REACT APP
+      <Input />
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import "./Input.css"
+import { useDispatch } from 'react-redux';
+import { saveTodo } from '../features/todoSlice';
+
+const Input = () => {
+
+    const [input, setInput] = useState("");
+    const dispatch = useDispatch()
+
+    const addToDo = () => {
+        console.log(`Adding ${input}`);
+        dispatch(saveTodo({
+            item: input,
+            done: false,
+            id: Date.now()
+        }))
+    }
+
+    return (
+        <div className="input">
+            <input 
+                className="input_todo"
+                type="text" 
+                value={input} 
+                onChange={e => setInput(e.target.value)} 
+            />
+            <button onClick={addToDo}>Add</button>
+        </div>
+    )
+}
+
+export default Input
